99e661450fd61ab7dbe226ae8876fef3c2330cb1
Lila Sicuan <new@mail.com>
2024-02-26 22:56:45 +0800

    Made client and master threads

--- master/MainMaster.java
+++ master/MainMaster.java
@@ -1,60 +1,133 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 import java.io.*;
 
 public class MainMaster {
-    private ServerSocket serverSocket = null;
-    private Socket socket = null;
-    private DataInputStream in = null;
-    private DataOutputStream out = null;
+    static ServerSocket serverSocket = null;
+    static int PORT = 1337;
+
+    static int start = 2;
+    static int end = 100;
+    static int NUM_THREADS = 1;
+
+    static ArrayList<Socket> slaves = new ArrayList<>();
+    static Semaphore readyToProcessSem = new Semaphore(-1);
+    static Semaphore slaveListSem = new Semaphore(1);
+
+  public static void main(String[] args) {
+    ClientThread client = new ClientThread();       // Gets user input
+    client.start();
+    MasterThread master = new MasterThread();       // Lets slaves connect
+    master.start();
+
+    // Make master not distribute tasks until there is at least one slave and user input is okay
+    while (true) {
+        try {
+            readyToProcessSem.acquire();
+
+            slaveListSem.acquire();
+            int numSlaves = slaves.size();
+            int size = end - start + 1;
+            numSlaves = Math.min(numSlaves, size);
+
+            int numPerSlave = size / numSlaves;
+            int tempStart = start;
+            int tempEnd = tempStart + numPerSlave - 1;
+
+            if(numPerSlave < 1) {
+                numPerSlave = 1;
+            }
+    
+            // TODO: figure out how to divide range
+            for (int i = 0; i < numSlaves; i++) {
+                tempStart = tempEnd + 1;
+
+                //Any excess goes to the final thread
+                if (i == numSlaves - 2) {
+                    tempEnd = end;
+                }
+                else
+                    tempEnd += numPerSlave;
+
+                // slaves.get(i)
+            }
+
+            slaveListSem.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+  }
+}
+
+// Client thread gets user input
+class ClientThread extends Thread {
     
 
-  public MainMaster(int port) {
-    try
-    {
-        serverSocket = new ServerSocket(port);
-        System.out.println("Server started");
+    public void run() {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Waiting for a client ...");
+            System.out.print("Enter start: ");
+            MainMaster.start = sc.nextInt();
+            System.out.print("Enter end: ");
+            MainMaster.end = sc.nextInt();
+            System.out.print("Enter number of threads: ");
+            MainMaster.NUM_THREADS = sc.nextInt();
 
-        //Accept multiple sockets learn how
-        socket = serverSocket.accept();
-        System.out.println("Client accepted");
+            sc.close();
 
-        // takes input from the client socket
-        in = new DataInputStream(System.in);
-        out = new DataOutputStream(socket.getOutputStream());
+            // Releases 1/2 of needed permits for processing to start
+            MainMaster.readyToProcessSem.release();
+
+            try {
+                // waits until request is fully processed
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    catch(UnknownHostException h) {
-        System.out.println(h);
-        return;
-    }
-    catch(IOException i) {
-        System.out.println(i);
-        return;
-    }
+}
 
-    String temp = "";
-
-    while(true) {
+// Keeps accepting connections from slaves
+class MasterThread extends Thread {
+    public void run() {
         try {
-            temp = in.readLine();
-            out.writeUTF(temp);
+            MainMaster.serverSocket = new ServerSocket(MainMaster.PORT);
+            System.out.println("Server started");
+
+            // Waits for at least one slave before releasing 1/2 needed permits for semaphore
+            System.out.println("Waiting for slave server/s to connect...");
+
+            MainMaster.slaveListSem.acquire();
+            MainMaster.slaves.add(MainMaster.serverSocket.accept());
+            MainMaster.slaveListSem.release();
+
+            MainMaster.readyToProcessSem.release();
+            System.out.println("New slave! Slave count: " + MainMaster.slaves.size());
+    
+            while(true) {
+                MainMaster.slaveListSem.acquire();
+                MainMaster.slaves.add(MainMaster.serverSocket.accept());
+                MainMaster.slaveListSem.release();
+
+                System.out.println("New slave! Slave count: " + MainMaster.slaves.size());
+            }
+        }
+        catch(UnknownHostException h) {
+            System.out.println(h);
+            return;
         }
         catch(IOException i) {
-            System.out.print(i);
+            System.out.println(i);
+            return;
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
-  }
-  
-  public static void main(String[] args) {
-    MainMaster server = new MainMaster(1337);
-  }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
